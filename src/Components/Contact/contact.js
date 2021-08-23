c2aa605b6e165ef88eee3745107198a563c7b5ab
elementarymindscape <sharma.vv13@gmail.com>
2021-08-23 17:02:57 +0530

    home and contact page done

--- src/Components/Contact/contact.js
+++ src/Components/Contact/contact.js
@@ -6,18 +6,19 @@ function Contact() {
         <div className="Contact" id="Contact">
             <div className='left'>
                 <div className='ImgContainer'>
-                <img src="assets/contactus2.jpg" alt="ContactPageImg" ></img>
+                <img src="assets/contactus2.jpg" alt="ContactUsImg" ></img>
                 </div>
             </div>
             <div className='right'>
                 <h1>Contact Us</h1>
+                <div className='grid-container'>
                 <form>
                     <input type="text" placeholder="Name"></input>
                     <input type="text" placeholder="Email"></input>
-                    <input type="text" placeholder="Subject"></input>
                     <textarea placeholder="Message"></textarea>
                     <button type="submit">SUBMIT</button>
                 </form>
+                </div>
             </div>
         </div>
     )
